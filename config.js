337c40d3e932979ddf3683b56525a73bd0f221be
developeronesw <developeronecms@gmail.com>
2026-09-23 20:50:04 -0400

    Add OneArtistHub Worker endpoint configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+window.OAH_CONFIG = Object.freeze({
+  siteUrl: "https://www.oneartisthub.site",
+  apiBaseUrl: "https://connect.oneartisthub.site",
+  endpoints: Object.freeze({
+    products: "/software/products",
+    checkout: "/software/checkout",
+    contact: "/software/contact",
+    webhook: "/software/webhook",
+    adminLogin: "/admin/login",
+    adminLogout: "/admin/logout",
+    adminOrders: "/admin/orders",
+    adminProducts: "/admin/products"
+  })
+});
